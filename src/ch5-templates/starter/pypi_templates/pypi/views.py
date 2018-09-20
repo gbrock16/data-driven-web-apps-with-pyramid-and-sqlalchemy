8a8e183c0b514de68de94e27579ce8e1fd2419d4
Garett Brock <gbrock16@gmail.com>
2018-09-19 21:33:28 -0700

    finished ch5 templates

--- src/ch5-templates/starter/pypi_templates/pypi/views.py
+++ src/ch5-templates/starter/pypi_templates/pypi/views.py
@@ -1,6 +1,20 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'Python Package Index'}
+def get_test_packages():
+    return [
+        {'name': 'requests', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.0.0'},
+        {'name': 'pyramid', 'version': '1.9.1'}
+    ]
+
+
+@view_config(route_name='home', renderer='templates/home_index.pt')
+def home_index(request):
+    return {
+        'packages': get_test_packages()
+    }
+@view_config(route_name='about', renderer='templates/home_about.pt')
+def about(_):
+    return {}
+
